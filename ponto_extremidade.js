1267ef502563c526e947b6c329a7890b82aa9a69
WilliamAB <wabbr@yahoo.com.br>
2025-02-25 16:12:45 -0300

    Update ponto_extremidade.js

--- ponto_extremidade.js
+++ ponto_extremidade.js
@@ -1,1 +1,25 @@
+   {
+  "input_data": {
+    "columns": [
+      "day",
+      "mnth",
+      "year",
+      "season",
+      "holiday",
+      "weekday",
+      "workingday",
+      "weathersit",
+      "temp",
+      "atemp",
+      "hum",
+      "windspeed"
+    ],
+    "index": [0],
+    "data": [[1,1,2022,2,0,1,1,2,0.3,0.3,0.3,0.3]]
+  }
+ }
 
+ [
+   Numero previsto:
+   352.3564674945718
+ ]
